b1f76dcb67118dec3ad0ef3fb732c0f0ccc92c24
Alena <Alena@10.2.4.170>
2018-04-05 08:41:58 +0200

    ukazka změny

--- src/server/Hrac.java
+++ src/server/Hrac.java
@@ -2,7 +2,6 @@ package server;
 import java.util.Calendar;
 import java.util.HashMap;
 public class Hrac {
-
 	private static int delay = 10000; //MILISECOND
 	private static HashMap<Integer, Hrac> pool = new HashMap<Integer, Hrac>();
 	
